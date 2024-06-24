78364f9c9848835b07d9a3d284db4c92525f764b
Harvindsokhal <harvindsokhal@gmail.com>
2024-06-24 01:05:28 +0100

    Normalise unit tests

--- object-orientated-programming/src/test/java/com/feefo/jobtitlenormaliser/NormaliserTest.java
+++ object-orientated-programming/src/test/java/com/feefo/jobtitlenormaliser/NormaliserTest.java
@@ -1,2 +1,15 @@
-package com.feefo.jobtitlenormaliser;public class NormaliserTest {
+package com.feefo.jobtitlenormaliser;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NormaliserTest {
+    @Test
+    public void testNormalise() {
+        Normaliser n = new Normaliser();
+        assertEquals("Software engineer", n.normalise("Java engineer"));
+        assertEquals("Software engineer", n.normalise("C# engineer"));
+        assertEquals("Accountant", n.normalise("Chief Accountant"));
+        assertEquals("Accountant", n.normalise("Accountant"));
+    }
 }
