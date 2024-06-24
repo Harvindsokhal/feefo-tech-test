7a34c15d910a79539c05436a5c674b2b1dc6b8f2
Harvindsokhal <harvindsokhal@gmail.com>
2024-06-24 01:29:07 +0100

    account-overview layout & styles

--- ui-assessment/src/account-overview.jsx
+++ ui-assessment/src/account-overview.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import { SalesOverview, SupportContact } from './components'
+
+const Container = styled.main`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 24px;
+`
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+`
+
+const Title = styled.h1`
+  font-weight: 600;
+  font-size: 28px;
+`
 
 export const AccountOverview = ({ data }) => {
   return (
-    <div className="AccountOverview">
-      <SupportContact />
-      <SalesOverview />
-    </div>
+    <Container>
+      <Header>
+        <Title>Account Overview</Title>
+        <SupportContact support={data.supportContact} />
+      </Header>
+      <SalesOverview sales={data.salesOverview} />
+    </Container>
   )
+}
+
+AccountOverview.propTypes = {
+  data: PropTypes.object.isRequired,
 }
 
 export default AccountOverview
